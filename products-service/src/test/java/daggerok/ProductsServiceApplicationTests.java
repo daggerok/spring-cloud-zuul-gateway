1c1c6aedbc1993d56ad9fa1d01d812c18f1a50bf
Maksim Kostromin <daggerok@gmail.com>
2017-05-02 02:18:50 +0300

    add random port to test web env

--- products-service/src/test/java/daggerok/ProductsServiceApplicationTests.java
+++ products-service/src/test/java/daggerok/ProductsServiceApplicationTests.java
@@ -6,11 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductsServiceApplicationTests {
 
   @Test
-  public void contextLoads() {
-  }
-
+  public void contextLoads() {}
 }
